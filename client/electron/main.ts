d6aae770d28c616f65660e99ee892c1fe88775ca
Body Fa <tcs223647@gmail.com>
2026-09-23 15:23:23 +0500

    fix: load electron updater in esm

--- client/electron/main.ts
+++ client/electron/main.ts
@@ -12,7 +12,8 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import crypto from "node:crypto";
-import { autoUpdater } from "electron-updater";
+import electronUpdater from "electron-updater";
+const { autoUpdater } = electronUpdater;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
